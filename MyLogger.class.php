b00ccece56d99daabec338bb9ec36495c60a4623
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-05-15 13:20:37 +0200

    Uitwerking van de exercise

--- MyLogger.class.php
+++ MyLogger.class.php
@@ -2,14 +2,22 @@
 
 class MyLogger
 {
-    public $standaardtekst = "ipsum";
-
+    public $standaardtekst1 = "INFO";
+    public $standaardtekst2 = "ERROR";
     function log($message)
     {
-        return ($message . $this->standaardtekst);
+        return ("Result " . $this->standaardtekst1 . " : " .$message);
+    }
+
+    function error($message){
+        return ("Result " . $this->standaardtekst2 . " : " .$message);
     }
 }
 
 $logger = new MyLogger();
-$antwoord = $logger->log("hello  ");
-print_r($antwoord);
+$antwoord1 = $logger->log('hello world', 'INFO'); // Result: 'INFO: hello world'
+print_r($antwoord1 . PHP_EOL);
+
+$antwoord2 = $logger->error('dit is een error'); // Result: 'ERROR: dit is een error'
+print_r($antwoord2);
+
