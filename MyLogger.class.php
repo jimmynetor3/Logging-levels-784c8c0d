76260723f75969c06ddc6f3d612c7a158df667ea
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-05-15 13:21:57 +0200

    Uitwerking van de exercise

--- MyLogger.class.php
+++ MyLogger.class.php
@@ -8,16 +8,15 @@ class MyLogger
     {
         return ("Result " . $this->standaardtekst1 . " : " .$message);
     }
-
-    function error($message){
+    function error($message)
+    {
         return ("Result " . $this->standaardtekst2 . " : " .$message);
     }
 }
-
 $logger = new MyLogger();
 $antwoord1 = $logger->log('hello world', 'INFO'); // Result: 'INFO: hello world'
 print_r($antwoord1 . PHP_EOL);
-
 $antwoord2 = $logger->error('dit is een error'); // Result: 'ERROR: dit is een error'
 print_r($antwoord2);
 
+?>
